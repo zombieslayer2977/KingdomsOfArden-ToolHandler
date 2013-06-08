d137757210e4869187e4f9803be349f35e1b386b
Andrew2060 <2060andrew@gmail.com>
2013-06-08 02:23:51 -0700

    Begin work on Item Spawn listening for automatic lore processing on item spawn
    Signed-off-by: Andrew2060 <2060andrew@gmail.com>

---
+++ src/net/swagserv/andrew2060/toolhandler/listeners/crafting/ItemSpawnListener.java
@@ -0,0 +1,10 @@
+package net.swagserv.andrew2060.toolhandler.listeners.crafting;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ItemSpawnEvent;
+
+public class ItemSpawnListener implements Listener {
+	public void onPickupItem(ItemSpawnEvent event) {
+		
+	}
+}
