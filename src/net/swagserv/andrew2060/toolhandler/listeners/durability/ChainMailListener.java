8884b19c368812fb72e3c03bf1234fca8c545e7a
Andrew2060 <2060andrew@gmail.com>
2013-06-14 07:11:05 -0700

    Begin Work on Chainmail Durability Damage Reduction

---
+++ src/net/swagserv/andrew2060/toolhandler/listeners/durability/ChainMailListener.java
@@ -0,0 +1,12 @@
+package net.swagserv.andrew2060.toolhandler.listeners.durability;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+public class ChainMailListener implements Listener {
+	@EventHandler(priority = EventPriority.LOWEST,ignoreCancelled = true) 
+	public void onDurabilityChange() {
+		
+	}
+}
