9fbfcb01b61ad4fa1842ea1dc2e4fd30323c255e
Andrew2060 <2060andrew@gmail.com>
2013-06-24 16:00:08 -0700

    Changes to Tool Lore

--- src/net/swagserv/andrew2060/toolhandler/util/GeneralLoreUtil.java
+++ src/net/swagserv/andrew2060/toolhandler/util/GeneralLoreUtil.java
@@ -17,7 +17,7 @@ public class GeneralLoreUtil {
 		templateLoreWeaponTool.add(1,ChatColor.WHITE + "=========Statistics==========");
 		templateLoreWeaponTool.add(2,ChatColor.GRAY + "Bonus True Damage: 0");
 		templateLoreWeaponTool.add(3,ChatColor.GRAY + "Bash Attack Chance: 0.00%");
-		templateLoreWeaponTool.add(4,ChatColor.GRAY + "Decapitation Chance: 0.00%");
+		templateLoreWeaponTool.add(4,ChatColor.GRAY + "Decimating Strike Chance: 0.00%");
 		templateLoreWeaponTool.add(5,ChatColor.WHITE + "========Modifications========");
 		templateLoreWeaponTool.add(6,ChatColor.DARK_GRAY + "[Empty Slot]");
 		templateLoreWeaponTool.add(7,ChatColor.DARK_GRAY + "[Empty Slot]");
