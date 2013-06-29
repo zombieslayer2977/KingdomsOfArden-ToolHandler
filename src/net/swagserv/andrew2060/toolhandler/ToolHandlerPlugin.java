5ce1d6b82aef3130308bf535400249ba545f10ea
Andrew2060 <2060andrew@gmail.com>
2013-06-29 16:54:04 -0700

    Update Version String to instigate updating

--- src/net/swagserv/andrew2060/toolhandler/ToolHandlerPlugin.java
+++ src/net/swagserv/andrew2060/toolhandler/ToolHandlerPlugin.java
@@ -52,7 +52,7 @@ public class ToolHandlerPlugin extends JavaPlugin{
 
 
     // Gets the 4 character version identifier associated with this version of tool lore to determine if an update is needed.
-    public static String versionIdentifier = ChatColor.AQUA + "" + ChatColor.AQUA + "" + ChatColor.RESET + "";
+    public static String versionIdentifier = ChatColor.AQUA + "" + ChatColor.BLACK + "" + ChatColor.RESET + "";
 
 	
 	public void onEnable() {
