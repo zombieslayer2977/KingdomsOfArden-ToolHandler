996492b2c1a761cae22d683a1e65bfd2259d033e
Andrew2060 <2060andrew@gmail.com>
2013-08-12 14:03:56 -0700

    Fix potion inventory update issues

--- src/main/java/net/kingdomsofarden/andrew2060/toolhandler/listeners/potions/PotionEffectManagerListener.java
+++ src/main/java/net/kingdomsofarden/andrew2060/toolhandler/listeners/potions/PotionEffectManagerListener.java
@@ -8,6 +8,7 @@ import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -44,26 +45,34 @@ public class PotionEffectManagerListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true) 
     public void onItemConsume(PlayerItemConsumeEvent event) {
         ItemStack i = event.getItem();
+        Player p = event.getPlayer();
         if(i.getType() == Material.POTION) {
             Potion pot = Potion.fromItemStack(i);
             Collection<PotionEffect> effects = pot.getEffects();
-            pEMan.addPotionEffectStacking(effects, event.getPlayer(), false);
+            pEMan.addPotionEffectStacking(effects, p, false);
         } else if(i.getType() == Material.GOLDEN_APPLE) {
             
             if(i.getData().getData() == 2) {    //Enchanted
-                pEMan.addPotionEffectStacking(PotionEffectType.REGENERATION.createEffect(1200, 2), event.getPlayer(), false);
+                pEMan.addPotionEffectStacking(PotionEffectType.REGENERATION.createEffect(1200, 2), p, false);
             } else {
-                pEMan.addPotionEffectStacking(PotionEffectType.REGENERATION.createEffect(1200, 1), event.getPlayer(), false);
+                pEMan.addPotionEffectStacking(PotionEffectType.REGENERATION.createEffect(1200, 1), p, false);
             }        
+            return; //We have our own seperate handling for golden apples where we don't want to remove quite yet or cancel the event
+            
         } else {
             return;
         }
        
         event.setCancelled(true);
         //Cancelling prevents item from being consumed, change/update ourselves
-        i.setType(Material.AIR);
-        i.setAmount(0);
-        event.getPlayer().updateInventory();
+        int amount = i.getAmount() - 1;
+        int handSlot = p.getInventory().getHeldItemSlot();
+        if(amount == 0) {
+            p.getInventory().clear(handSlot);
+        } else {
+            i.setAmount(amount);
+        }
+        p.updateInventory();
     }
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPotionSplash(PotionSplashEvent event) {
